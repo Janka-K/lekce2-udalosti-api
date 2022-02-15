59388b48a1b50551f0b6a56d1651d3c90fbe682a
Janka K <janakorycanova@gmail.com>
2022-02-15 20:00:32 +0100

    zpracovani ukolu behem hodiny + zacatek povinneho  ukolu

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 // tady je místo pro náš program
 
-let nadpis = document.querySelector('h1');
-nadpis.classList.add('.zluty'); //pridani tridy
-nadpis.classList.remove('zluty'); //odebrani tridy
+let nadpis = document.querySelector("h1");
+nadpis.classList.add(".zluty"); //pridani tridy
+nadpis.classList.remove("zluty"); //odebrani tridy
 
-let ctverecek = document.querySelector('div.zluty');
-ctverecek.classList.add('ctverecek');
+let ctverecek = document.querySelector("div.zluty");
+ctverecek.classList.add("ctverecek");
 
 let pocitadlo = 0;
 
-
-
-
-function priNajetiNaCtverec(){
-    console.log("test" + pocitadlo);
-    pocitadlo ++; //zvednuti hodnoty v promenne o 1
+function priNajetiNaCtverec() {
+  console.log("test" + pocitadlo);
+  pocitadlo++; //zvednuti hodnoty v promenne o 1
 }
 
-function zmenStyl(){
-    nadpis.classList.toggle('zeleny'); // priklad na toggle, pri kliku se nadpis obarvi zelene, pokud jiz zeleny je, pak se zelena ztrati
+function zmenStyl() {
+  nadpis.classList.toggle("zeleny"); // priklad na toggle, pri kliku se nadpis obarvi zelene, pokud jiz zeleny je, pak se zelena ztrati
+}
+
+let odstavec = document.querySelector("p");
+odstavec.style.fontSize = "16px";
+
+function priNajetiNaText() {
+  let text = document.querySelector("p");
+  text.style.fontWeight = "bold";
 }
