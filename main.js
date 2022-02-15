d5cbc0705159d91ab093bcf865ecd20554bce659
Janka K <janakorycanova@gmail.com>
2022-02-15 18:51:28 +0100

    Removed console.log and testing classList.add a classList.toggle

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 // tady je místo pro náš program
 
-console.log("Ahoj svete!!")
+let nadpis = document.querySelector('h1');
+nadpis.classList.add('.zluty'); //pridani tridy
+nadpis.classList.remove('zluty'); //odebrani tridy
+
+let ctverecek = document.querySelector('div.zluty');
+ctverecek.classList.add('ctverecek');
+
