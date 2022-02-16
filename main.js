c5cc7861bf6f9c1709ec8f746f8da77de1fc48c9
Janka K <janakorycanova@gmail.com>
2022-02-16 21:16:39 +0100

    vypracovani prvnich dvou ukolu-bude treba jeste doladit

--- main.js
+++ main.js
@@ -18,10 +18,31 @@ function zmenStyl() {
   nadpis.classList.toggle("zeleny"); // priklad na toggle, pri kliku se nadpis obarvi zelene, pokud jiz zeleny je, pak se zelena ztrati
 }
 
-let odstavec = document.querySelector("p");
-odstavec.style.fontSize = "16px";
-
 function priNajetiNaText() {
   let text = document.querySelector("p");
   text.style.fontWeight = "bold";
+}
+
+function priOdjetiZTextu() {
+  let text = document.querySelector("p");
+  text.style.fontWeight = "normal";
+}
+
+function startAudio() {
+  // vyvolani audia na strance
+  let audioFile = document.getElementById("#zvukovaStopa");
+  console.log("Zapinam hudbu");
+  audioFile.play();
+}
+
+function obarviCervene() {
+  let text = document.querySelector("p");
+  text.classList.toggle("cervena");
+}
+
+let counter = 16; //zde by slo jeste zlepsit nacitani font-size z elementu
+
+function zvetseniOPixel() {
+  let text = document.querySelector("p");
+  text.style.fontSize = counter++ + "px";
 }
